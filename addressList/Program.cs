ce7e60b3c0970957b87c5032f13016e58d848277
jandershock <jordan.andershock@gmail.com>
2022-05-24 09:25:13 -0500

    Finished book exercise

---
+++ addressList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressListExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> randomList = new List<string>();
+
+            randomList.Add("123 Street");
+            randomList.Add("222 Road");
+            randomList.Add("112 Blvd");
+            randomList.Add("Yo boy its a road");
+
+            foreach (string el in randomList)
+            {
+                Console.WriteLine(el);
+            }
+
+            List<Address> listOfAddressClasses = new List<Address>();
+            listOfAddressClasses.Add(new Address("123 Street"));
+            listOfAddressClasses.Add(new Address("222 Road"));
+            listOfAddressClasses.Add(new Address("112 Blvd"));
+            listOfAddressClasses.Add(new Address("Yo boy its a road"));
+
+            foreach (Address address in listOfAddressClasses)
+            {
+                address.printAddress();
+            }
+        }
+
+    }
+
+    public class Address
+    {
+        private string givenAddress;
+
+        public Address(string input)
+        {
+            givenAddress = input;
+        }
+
+        public void printAddress()
+        {
+            Console.WriteLine($"You're in an Address Class! {givenAddress}");
+        }
+    }
+
+
+
+}
---
+++ book/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+Book book1 = new Book() {
+    Author = "J. R. R. Tolkien",
+    Title = "The Fellowship of the Ring"
+};
+
+Book book2 = new Book() {
+    Author = "J. R. R. Tolkien",
+    Title = "The Two Towers"
+};
+
+Book book3 = new Book() {
+    Author = "J. R. R. Tolkien",
+    Title = "The Return of the King"
+};
+
+List<Book> myLibrary = new List<Book>();
+
+myLibrary.AddRange(new List<Book>(){
+    book1,
+    book2,
+    book3
+});
+
+foreach(Book book in myLibrary)
+{
+    Console.WriteLine($"{book.Title} by {book.Author}");
+}
+
+class Book
+{
+    public string Author { get; set; }
+    public string Title { get; set; }
+}
